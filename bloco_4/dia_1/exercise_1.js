e0a4ef0bdf8dda7fff5b6f5b5e009ad366023b44
Rodrigo Farias <deltadigo@gmail.com>
2020-11-26 13:02:03 -0300

    Exercicio 1 - Criado 5 programas com operações aritiméticas

---
+++ bloco_4/dia_1/exercise_1.js
@@ -0,0 +1,8 @@
+let a = 10;
+let b = 15;
+
+console.log('Adição de a e b: ' + (a + b));
+console.log('Subtração de a e b: ' + (a - b));
+console.log('Multiplicação de a e b: ' + (a * b));
+console.log('Divisão de a e b: ' + (a / b));
+console.log('Módulo de a e b: ' + (a % b));
