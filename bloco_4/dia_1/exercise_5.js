3519d6ff92297ecf522bb291eaf6787cdcfe7fc3
Rodrigo Farias <deltadigo@gmail.com>
2020-11-28 18:11:19 -0300

    Criado lógica para verificar se é um triângulo baseado em 3 angulos informados

---
+++ bloco_4/dia_1/exercise_5.js
@@ -0,0 +1,9 @@
+let anguloA = 40;
+let anguloB = 40;
+let anguloC = 80;
+
+if (anguloA + anguloB + anguloC === 180) {
+  console.log('É um triângulo')
+} else {
+  console.log('Erro: Os valores correspondentes não formam um triângulo.')
+}
