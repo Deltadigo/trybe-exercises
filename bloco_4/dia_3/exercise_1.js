07fc14b513b206c914b279106a4c894ed6081706
Rodrigo Farias <deltadigo@gmail.com>
2020-11-29 11:54:58 -0300

    Adicionado ao arquivo 1 lógica para impressão de um quadrado com asteriscos

---
+++ bloco_4/dia_3/exercise_1.js
@@ -0,0 +1,10 @@
+let n = 5;
+let asteriscos = ''
+
+for (let index = 1; index <= n; index += 1) {
+  asteriscos += '*'
+}
+
+for (let index = 1; index <= n; index += 1) {
+  console.log(asteriscos)
+}
